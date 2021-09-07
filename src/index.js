e6ce66c40ee65ad85985367f622912b0f1315d9b
anaisalfonsi <alfonsi_anais@hotmail.fr>
2021-09-07 16:53:32 +0200

    2d commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+// stateless functional component
+// always return jsx
+
 function Greeting() {
-  return <h4>Hi I'm Anaïs and this is my first component</h4>
+  return (
+    <div>
+      <h4>Hi I'm Anaïs and this is my first component</h4>
+    </div>
+  );
 }
+
+/* const Greeting = () => {
+  return React.createElement("h1", {}, "hello world");
+}; */
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
