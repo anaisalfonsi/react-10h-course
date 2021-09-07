76394b2d9e583b5c3cf396b804fb99d2a003ee87
anaisalfonsi <alfonsi_anais@hotmail.fr>
2021-09-07 18:30:03 +0200

    phase two done

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
 import React from "react";
 import ReactDom from "react-dom";
+import './index.css';
 
-// stateless functional component
-// always return jsx
-
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-const Person = () => <h2>john doe</h2>;
-const Message = () => {
-  return <p>this is my message</p>;
+const Book = () => {
+  return <article>
+    <Image />
+    <Title />
+    <Author />
+  </article>
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Image = () => (
+  <img
+    src="https://images-eu.ssl-images-amazon.com/images/I/813MhRXz2CL._AC_UL200_SR200,200_.jpg"
+    alt=""
+  />
+);
+
+const Title = () => (
+  <h1>La Princesse de Clèves</h1>
+);
+
+const Author = () => <h4>Mme de Lafayette</h4>;
+
+ReactDom.render(<BookList />, document.getElementById("root"));
